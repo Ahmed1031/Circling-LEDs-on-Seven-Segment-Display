793653859bd08ec402aeafc3b6e51b7bbf31c098
Ahmed Asim Ghouri <74955488+Ahmed1031@users.noreply.github.com>
2021-10-20 12:36:17 +0500

    Update tb_ssd.vhd
    Added HEX0 expected output and check mechanism

--- tb_ssd.vhd
+++ tb_ssd.vhd
@@ -44,6 +44,7 @@ end component;
 	signal	hex5_tb     :  std_logic_vector(6 downto 0):=(others=>'0');
 	signal	hex6_tb     :  std_logic_vector(6 downto 0):=(others=>'0');
 	signal	hex7_tb     :  std_logic_vector(6 downto 0):=(others=>'0');
+	signal   expected    :  std_logic_vector(6 DOWNTO 0):=(others=>'0');
 	constant clk_period :time := 20ns;
 --------------------------------------------------------
 begin
@@ -64,13 +65,53 @@ UUT_ssd:ssd PORT MAP
 ----------------------------
 
  key_tb(0) <= '0', '1' after 50 ns; 
- 
+-----------------------------------
+ --- Generate template:
+ -- Expected output for hex0 ssd 
+ expected <= "0111111" AFTER 21ns,
+				 "0011111" AFTER 120000061ns,
+				 "1011111" AFTER 160000061ns,
+				 "1001111" AFTER 280000061ns,
+				 "1101111" AFTER 320000061ns,
+				 "1100111" AFTER 440000061ns,
+				 "1110111" AFTER 480000061ns,
+				 "1110011" AFTER 600000061ns,
+				 "1111011" AFTER 640000061ns,
+				 "1111001" AFTER 760000061ns,
+				 "1111101" AFTER 800000061ns,
+				 "0111101" AFTER 920000061ns,
+				 "1111111" AFTER 960000061ns;
+-----------------------------------------
 -- Clock Gen
 process(clk_tb)
   begin 
 	 clk_tb <= not clk_tb after clk_period/2;
 end process;
-----------------------------------------------	
+----------------------------------------------
+---Make comparison:
+ PROCESS
+ 
+    BEGIN
+	 
+      WAIT FOR 50ns;
+		
+        IF (NOW<960997937ns) THEN
+		  
+           ASSERT (hex0_tb = expected)
+			  
+				 REPORT "Mismatch at t=" & TIME'IMAGE(NOW) &
+				 " hex0_tb =" & INTEGER'IMAGE(conv_integer(hex0_tb)) &
+				 " expexted =" & INTEGER'IMAGE(conv_integer(expected))
+             SEVERITY FAILURE;
+				 
+        ELSE
+		  
+             ASSERT FALSE
+				 
+						REPORT "No error found (t=" & TIME'IMAGE(NOW) & ")"
+                  SEVERITY NOTE;
+        END IF;
+ END PROCESS;	
 end test_display; 	  
 	  
 	  
